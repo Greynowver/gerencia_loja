f6724e598ca17826e7e1e24ccba4aa58d38b60ef
Greynowver <greynowver@gmail.com>
2020-01-14 17:08:34 -0300

    BLoC E Validators

---
+++ lib/validators/login_validators.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+
+class LoginValidators {
+
+  final validateEmail = StreamTransformer<String, String>.fromHandlers(
+    handleData: (email, sink) {
+      if (email.contains("@")) {
+        sink.add(email);
+      }
+      else {
+        sink.addError("Insira i, e-mail válido");
+      }
+    }
+  );
+
+  final validatePassword = StreamTransformer<String, String>.fromHandlers(
+    handleData: (password, sink) {
+      if(password.length >= 6){
+        sink.add(password);
+      } else {
+        sink.addError("A senha deve conter pelo menos 6 caracteres");
+      }
+    }
+  );
+
+}
